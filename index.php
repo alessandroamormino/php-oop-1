dfb55429fa791636a4562026383dce319c1bde42
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-03 15:52:58 +0200

    feat: create db.php file

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php 
 // importo la classe Movie
 require_once './Models/Movie.php';
-
-// istanzio due oggetti Movie
-$guardians = new Movie("Guardians Of The Galaxy Vol. 3", ['Adventure', 'Action', 'Comedy'], "2h 30m", 8, "Still reeling from the loss of Gamora, Peter Quill rallies his team to defend the universe and one of their own - a mission that could mean the end of the Guardians if not successful.");
-
-$johnWick = new Movie("John Wick: Chapter 4", ['Action', 'Crime', 'Thriller'], "2h 49m", 8, "Condemned by the tyrannical High Table to be on the run for the rest of his life, deadly assassin maestro John Wick (2014) embarks on a Sisyphean mission of suicidal fury to decide his fate after the merciless carnage in John Wick: Chapter 3 - Parabellum (2019).");
+require_once 'db.php';
 
 
 ?>
