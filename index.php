a9b502362236c6bf33bdc85864c1a75ac060db3f
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-03 17:05:46 +0200

    style: cards

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'db.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="it">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,6 +14,10 @@ require_once 'db.php';
   <title>PHP OOP 1</title>
   <!-- css -->
   <link rel="stylesheet" href="css/style.css">
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 </head>
 <body>
   <h1>Movies</h1>
@@ -24,31 +28,34 @@ require_once 'db.php';
       ?>
       <div class="card">
         <div class="title">
-          <h3><?= $singleMovie->title ?></h3>
+          <h2><?= $singleMovie->title ?></h2>
         </div>
         <div class="image">
           <img src="<?= $singleMovie->image ?>" alt="<?= $singleMovie->title ?>">
         </div>
-        <div class="genres">
-          <!-- stampo più generi separati da una virgola -->
-          <?php 
-            $count = count($singleMovie->genre) - 1;
-            foreach($singleMovie->genre as $key=>$singleGenre){
-              echo $singleGenre;
-              if($key < $count){
-                echo ", ";
+        <div class="infos">
+          <div class="genres">
+            <!-- stampo più generi separati da una virgola -->
+            Genre: 
+            <?php 
+              $count = count($singleMovie->genre) - 1;
+              foreach($singleMovie->genre as $key=>$singleGenre){
+                echo $singleGenre;
+                if($key < $count){
+                  echo ", ";
+                }
               }
-            }
-          ?>
-        </div>
-        <div class="duration">
-          <?= $singleMovie->duration ?>
-        </div>
-        <div class="ratings">
-          <?= $singleMovie->getInfo(); ?> 
-        </div>
-        <div class="storyline">
-          <?= $singleMovie->storyline ?>
+            ?>
+          </div>
+          <div class="duration">
+            Duration: <?= $singleMovie->duration ?>
+          </div>
+          <div class="ratings">
+            Rating: <?= $singleMovie->getInfo(); ?> 
+          </div>
+          <div class="storyline">
+            Storyline: <?= $singleMovie->storyline ?>
+          </div>
         </div>
       </div>
       <?php
